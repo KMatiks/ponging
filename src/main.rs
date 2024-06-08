2d0fa9dd6db197596f1cdba0761844ab8a8d6e6a
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-06-08 18:53:49 -0400

    Add 2 paddles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,58 @@
-use bevy::{prelude::*, window::*};
+use bevy::{prelude::*, sprite::*, window::*};
 
+
+/* Entity
+ - Paddle
+*/
+
+//Paddle component - identify entity that has this component is a paddle
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
+struct Player1;
+
+#[derive(Component)]
+struct Player2;
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+
+    let shape = Mesh2dHandle(meshes.add(Rectangle::new(50., 100.)));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: shape,
+        material: materials.add(Color::rgb(255., 255., 255.)),
+        transform: Transform::from_xyz(
+            0.0,
+            0.0,
+            0.0,
+        ),
+        ..default()
+    })
+    .insert(Paddle)
+    .insert(Player1);
+
+    let shape = Mesh2dHandle(meshes.add(Rectangle::new(50., 100.)));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: shape,
+        material: materials.add(Color::rgb(255., 255., 255.)),
+        transform: Transform::from_xyz(
+            90.0,
+            0.0,
+            0.0,
+        ),
+        ..default()
+    })
+    .insert(Paddle)
+    .insert(Player2);
+}
 
 fn main() {
     App::new()
@@ -11,5 +64,6 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, (spawn_camera, spawn_paddles))
         .run();
 }
