23d2bb88b715174c33465d3eb2ddacfa628c0621
Khoa Hoang <koadahoakodah@gmail.com>
2024-06-10 01:42:19 -0400

    Fix windows terminal error spam due to bug with wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::*, window::*, math::bounding::*};
+use bevy::{math::bounding::*, prelude::*, render::{settings::{Backends, RenderCreation, WgpuSettings}, RenderPlugin}, sprite::*, window::*};
 use rand::Rng;
 
 
@@ -176,14 +176,27 @@ fn handle_collisions(
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: WindowResolution::new(WIDTH, HEIGHT).with_scale_factor_override(1.0),
-                resizable: false,
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WindowResolution::new(WIDTH, HEIGHT).with_scale_factor_override(1.0),
+                    resizable: false,
+                    ..default()
+                }),
                 ..default()
-            }),
-            ..default()
-        }))
+            })
+            //------------------------------------------------------------------
+            // Fix for windows terminal error spam due to wgpu bug
+            // See this issue: https://github.com/bevyengine/bevy/issues/9975
+            .set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    #[cfg(target_os = "windows")]
+                    backends: Some(Backends::VULKAN),
+                    ..default()
+                }),
+                ..default()
+            //------------------------------------------------------------------
+            }))
         .add_systems(Startup, (spawn_camera, spawn_paddles, spawn_ball))
         .add_systems(Update, (apply_velocity, handle_keyboard_input, handle_gamepad_input, handle_collisions))
         .run();
